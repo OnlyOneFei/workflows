af08c76b114a7830158efd9810092f12973e66e7
FeiYu <844797191@qq.com>
2022-08-26 15:52:33 +0800

    初始化

---
+++ src/main/java/com/fy/workflows/WorkflowsApplication.java
@@ -0,0 +1,13 @@
+package com.fy.workflows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkflowsApplication.class, args);
+    }
+
+}
